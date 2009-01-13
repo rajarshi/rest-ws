bb5512d6fae56e9574e619149ecae933f3b3fda4
Rajarshi  Guha <rajarshi.guha@gmail.com>
2009-01-13 13:42:54 -0500

    updated predict service to employ rpy2. Also added the start of a README describing the services

--- predict/predictors.py
+++ predict/predictors.py
@@ -3,11 +3,19 @@ import SOAPpy, sys, string, StringIO, base64, urllib
 import elementtree.ElementTree as ET
 from elementtree.ElementTree import XML
 
+import rpy2.robjects as ro
+rinterp = ro.r
 
 baseUrl = 'http://rguha.ath.cx/~rguha/cicc/rest/desc/descriptors/org.openscience.cdk.qsar.descriptors.molecular.'
 descs = {'ATSm1' : 'AutocorrelationDescriptorMass',
          'C1SP2' : 'CarbonTypesDescriptor',
          'AMR' : 'ALOGPDescriptor' }
+
+def _descDictToDataFrame(d):
+    newd = {}
+    for key in d.keys():
+        newd[key] = ro.FloatVector([d[key]])
+    return rinterp['data.frame'](**newd)
 
 def handler(req):
     uriParts = req.uri.split('/')
@@ -25,20 +33,11 @@ def handler(req):
         for value in values:
             if value.attrib['name'] == key: descriptors[key] = float(value.attrib['value'])
 
+    ## convert desc dict to data.frame
+    descdf = _descDictToDataFrame(descriptors)
+
     ## load the model and get a prediction
-    from rpy import *
-
-    makeDF = r("""
-function( ..., names) 
-{ 
-df <- data.frame(...)
-names(df) <- names
-df
-}
-""")
-
-    df = makeDF(1,2,3, names= ['a','b','c'])
-#    r("""load('test-model.R')""")
-
-    req.write(str(descriptors))
+    #rinterp("""load('/Users/rguha/src/rest-ws/predict/test-model.Rda')""")
+    rinterp.load('/Users/rguha/src/rest-ws/predict/test-model.Rda')
+    req.write(descdf.colnames().r_repr())
     return apache.OK
